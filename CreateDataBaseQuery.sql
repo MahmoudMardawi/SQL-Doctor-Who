3a2f54524771187fa21fe1e971b4a105c0eb64b6
MahmoudMardawi <46374856+MahmoudMardawi@users.noreply.github.com>
2022-05-25 23:04:37 +0300

    initial commit

---
+++ CreateDataBaseQuery.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS DoctorWho;
+GO
+
+CREATE DATABASE DoctorWho;
+GO
+
+USE DoctorWho;
+GO
+
+drop table if exists tblEnemy;
+drop table if exists tblAuthor;
+drop table if exists tblDoctor;
+drop table if exists tblCompanion;
+drop table if exists tblEpisode;
+drop table if exists tblEpisodeCompanion;
+drop table if exists tblEpisodeEnemy;
+
+
+
