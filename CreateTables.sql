b43b3fc78d4f4b7cdcb3a28ff6bbd4ef0f458042
Mahmoud Mardawi <mahmoud.mardawi@foothillsolutions.com>
2022-05-26 16:05:34 +0300

    Create tables and keys and relationships

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,10 +1,10 @@
 DROP DATABASE IF EXISTS DoctorWho;
 GO
 
-CREATE DATABASE DoctorWho;
+CREATE DATABASE DoctorWhoCore;
 GO
 
-USE DoctorWho;
+USE DoctorWhoCore;
 GO
 
 drop table if exists tblEnemy;
@@ -26,6 +26,20 @@ AuthorId int identity primary key not null,
 AuthorName nvarchar(50) not null,
 );
 
+Create table tblCompanion(
+CompanionId int identity primary key not null,
+CompanionName nvarchar(50) not null,
+WhoPlayed nvarchar(50) not null
+);
+
+create table tblDoctor(
+DoctorId int identity primary key not null,
+DoctorName nvarchar(50) not null,
+BirthDate date null default null,
+FirstEpisodeDate date null default null,
+LastEpisodeDate date null default null
+);
+
 create table tblEpisode(
 EpisodeId int primary key not null,
 SeriesNumber int null,
@@ -34,14 +48,19 @@ EpisodeType VARCHAR(250) NOT NULL,
 Title Text NOT NULL,
 EpisodeDate Date NULL DEFAULT NULL,
 AuthorId INT NOT NULL REFERENCES tblAuthor (AuthorId),
-DoctorId VARCHAR(250) NOT NULL REFERENCES tblDoctor (DoctorId),
+DoctorId int not null references tblDoctor (DoctorId),
 Notes Text NULL DEFAULT NULL
 );
 
 create table tblEpisodeEnemy(
 EpisodeEnemyId int identity primary key not null,
 EpisodeId int not null references tblEpisode(EpisodeId),
-EnemyId int foreign key references tblEnemy(EnemyId)
+EnemyId int not null references tblEnemy(EnemyId)
 );
 
+create table tblEpisodeCompanion(
+EpisodeCompanionId int identity primary key not null,
+EpisodeId int not null references tblEpisode(EpisodeId),
+CompanionId int not null references tblCompanion(CompanionId)
+);
 
